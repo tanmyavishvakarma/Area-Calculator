444aba75e93e08dab37ea6107aa38628a1da99ed
Tanmya <tanmya2000@gmail.com>
2017-04-11 03:55:07 -0700

    Area of circle version 2.0

---
+++ Area_Of_Circle_Ver2/src/area_of_circle_ver2/Area_Of_Circle_Ver2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package area_of_circle_ver2;
+
+/**
+ *
+ * @author TANMYA THE GREAT
+ */
+public class Area_Of_Circle_Ver2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+    }
+
+}
